b305de302d19703a6da9acaf47c650241e723c8e
Vishwanath C <meetvishwa123@rediffmail.com>
2023-02-17 12:22:17 +0530

    Update stringProblems.js

--- stringProblems.js
+++ stringProblems.js
@@ -7,3 +7,23 @@ function reverseString(str) {
 console.log(reverseString("hello")); // Output: "olleh"
 
 //------------------------------------------------------------------------------------------------------------------------------
+
+2) Write a function called findLongestWord that takes a string as an argument and returns the length of the longest word in the string. For example, if the input is "The quick brown fox jumps over the lazy dog", the output should be 5 (the length of the longest word, which is "jumps").
+
+function findLongestWord(str) {
+  const words = str.split(" ");
+  let maxLength = 0;
+  
+  for (let i = 0; i < words.length; i++) {
+    const wordLength = words[i].length;
+    if (wordLength > maxLength) {
+      maxLength = wordLength;
+    }
+  }
+  
+  return maxLength;
+}
+
+console.log(findLongestWord("The quick brown fox jumps over the lazy dog")); // Output: 5
+
+//-----------------------------------------------------------------------------------------------------------------------------------
