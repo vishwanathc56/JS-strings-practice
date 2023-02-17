86d1dab02a5142a0d4cd609e0df3dcafaf00f02e
Vishwanath C <meetvishwa123@rediffmail.com>
2023-02-17 12:19:26 +0530

    Update stringProblems.js

--- stringProblems.js
+++ stringProblems.js
@@ -1,1 +1,9 @@
+1) Write a function called reverseString that takes a string as an argument and returns the reverse of that string. For example, if the input is "hello", the output should be "olleh".
 
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(reverseString("hello")); // Output: "olleh"
+
+//------------------------------------------------------------------------------------------------------------------------------
