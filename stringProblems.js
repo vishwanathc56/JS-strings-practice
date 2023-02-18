b7a7e90c05a0e740b38e29d43407b2c45f916b20
Vishwanath C <meetvishwa123@rediffmail.com>
2023-02-18 23:54:02 +0530

    Update stringProblems.js

--- stringProblems.js
+++ stringProblems.js
@@ -68,4 +68,20 @@ function countVowels(str) {
 
 console.log(countVowels("hello world")); // Output: 3
 
+//                      alternative
+
+function countVowels(str) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  let count = 0;
+  
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i].toLowerCase())) {
+      count++;
+    }
+  }
+  
+  return count;
+}
+
+console.log(countVowels("hello world")); // Output: 3
 
