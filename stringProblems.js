c21876845a2aa60fdc1fe4886608bc3d8ce362c9
Vishwanath C <meetvishwa123@rediffmail.com>
2023-02-20 22:00:59 +0530

    Update stringProblems.js

--- stringProblems.js
+++ stringProblems.js
@@ -206,3 +206,18 @@ function isAnagram(str1, str2) {
 console.log(isAnagram("listen", "silent")); // Output: true
 console.log(isAnagram("hello", "world")); // Output: false
 
+//                    alternative
+
+function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  const sortedStr1 = str1.split("").sort().join("");
+  const sortedStr2 = str2.split("").sort().join("");
+
+  return sortedStr1 === sortedStr2;
+}
+
+console.log(isAnagram("listen", "silent")); // Output: true
+console.log(isAnagram("hello", "world")); // Output: false
