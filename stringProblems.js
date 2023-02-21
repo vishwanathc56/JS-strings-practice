399679eb07f85f6b8a6d54d1f27d7accba35fbf2
Vishwanath C <meetvishwa123@rediffmail.com>
2023-02-22 01:43:19 +0530

    Update stringProblems.js

--- stringProblems.js
+++ stringProblems.js
@@ -247,5 +247,18 @@ console.log(isAnagram("hello", "world")); // Output: false
       return unique;
      }
 
-    console.log(reverseWords(["apple", "banana", "cherry", "apple", "banana"]));
+    //                    alternative
+
+    function removeDuplicates(arr) {
+      const result = [];
+      for (let i = 0; i < arr.length; i++) {
+        if (!result.includes(arr[i])) {
+          result.push(arr[i]);
+        }
+      }
+      return result;
+    }
+
+    console.log(removeDuplicates(["apple", "banana", "cherry", "apple", "banana"])); // Output: ["apple", "banana", "cherry"]
+
      
